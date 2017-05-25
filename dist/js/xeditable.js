3b7f1d14aa96f0b9957191b7f137b2373e81043d
Tatyana Burlakova <tburlakova@odin.com>
2017-05-25 13:59:11 +0300

    error massage change position 2

--- dist/js/xeditable.js
+++ dist/js/xeditable.js
@@ -572,7 +572,7 @@ angular.module('xeditable').factory('editableController',
 
       //build error
       self.errorEl = angular.element(theme.errorTpl);
-      self.controlsEl.prepend(self.errorEl);
+      self.buttonsEl.prepend(self.errorEl);
 
       //build editor
       self.editorEl = angular.element(self.single ? theme.formTpl : theme.noformTpl);
